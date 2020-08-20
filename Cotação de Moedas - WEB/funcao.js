750dbf439d21af5d614868b4ab2252255065b263
ThiagoLozano <51161587+ThiagoLozano@users.noreply.github.com>
2020-08-20 19:08:23 -0300

    Add files via upload

---
+++ Cotação de Moedas - WEB/funcao.js
@@ -0,0 +1,58 @@
+function validar(){
+    var item = '';
+    item = document.getElementById('1');
+    src = item.getAttribute('src');
+    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="BitCoin">
+    <form action="" id="dados">
+        Valor em Reais R$: <input type="text" id="cidade" disabled size="30" placeholder="R$"><br>
+    </form>`;
+}
+
+function validar2(){
+    var item = '';
+    item = document.getElementById('2');
+    src = item.getAttribute('src');
+    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
+}
+
+function validar3(){
+    var item = '';
+    item = document.getElementById('3');
+    src = item.getAttribute('src');
+    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
+}
+
+function validar4(){
+    var item = '';
+    item = document.getElementById('4');
+    src = item.getAttribute('src');
+    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
+}
+
+function validar5(){
+    var item = '';
+    item = document.getElementById('5');
+    src = item.getAttribute('src');
+    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
+}
+
+function validar6(){
+    var item = '';
+    item = document.getElementById('6');
+    src = item.getAttribute('src');
+    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
+}
+
+function validar7(){
+    var item = '';
+    item = document.getElementById('7');
+    src = item.getAttribute('src');
+    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
+}
+
+function validar8(){
+    var item = '';
+    item = document.getElementById('8');
+    src = item.getAttribute('src');
+    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
+}
