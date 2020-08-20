cca4988886faf3129dd8c0b03382288dc83b404b
ThiagoLozano <51161587+ThiagoLozano@users.noreply.github.com>
2020-08-20 19:11:41 -0300

    JS

--- Cotação de Moedas - WEB/funcao.js
+++ Cotação de Moedas - WEB/funcao.js
@@ -1,58 +1,12 @@
 function validar(){
     var item = '';
+    // Pega o valor do ID.
     item = document.getElementById('1');
+    // Busca o atributo 'SRC'.
     src = item.getAttribute('src');
+    // Substitui o valor da HTML.
     document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="BitCoin">
     <form action="" id="dados">
         Valor em Reais R$: <input type="text" id="cidade" disabled size="30" placeholder="R$"><br>
     </form>`;
-}
-
-function validar2(){
-    var item = '';
-    item = document.getElementById('2');
-    src = item.getAttribute('src');
-    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
-}
-
-function validar3(){
-    var item = '';
-    item = document.getElementById('3');
-    src = item.getAttribute('src');
-    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
-}
-
-function validar4(){
-    var item = '';
-    item = document.getElementById('4');
-    src = item.getAttribute('src');
-    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
-}
-
-function validar5(){
-    var item = '';
-    item = document.getElementById('5');
-    src = item.getAttribute('src');
-    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
-}
-
-function validar6(){
-    var item = '';
-    item = document.getElementById('6');
-    src = item.getAttribute('src');
-    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
-}
-
-function validar7(){
-    var item = '';
-    item = document.getElementById('7');
-    src = item.getAttribute('src');
-    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
-}
-
-function validar8(){
-    var item = '';
-    item = document.getElementById('8');
-    src = item.getAttribute('src');
-    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="">`;
 }
