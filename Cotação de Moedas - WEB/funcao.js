ed0b0a0ddbfa2d4a079bc33bc6193ccebaa63021
ThiagoLozano <51161587+ThiagoLozano@users.noreply.github.com>
2020-08-21 16:50:19 -0300

    JS

--- Cotação de Moedas - WEB/funcao.js
+++ Cotação de Moedas - WEB/funcao.js
@@ -1,12 +1,152 @@
-function validar(){
-    var item = '';
-    // Pega o valor do ID.
-    item = document.getElementById('1');
-    // Busca o atributo 'SRC'.
-    src = item.getAttribute('src');
-    // Substitui o valor da HTML.
-    document.getElementById('imgMoeda').innerHTML = `<img src="${src}" alt="BitCoin">
-    <form action="" id="dados">
-        Valor em Reais R$: <input type="text" id="cidade" disabled size="30" placeholder="R$"><br>
-    </form>`;
-}
+function validar() {
+    // Troca a logo pela Imagem da moeda
+    var moeda = document.getElementById('1').src;
+    document.getElementById('logo').src = moeda;
+
+    // Mostra a cotação.
+    var url = 'https://economia.awesomeapi.com.br/all/BTC-BRL'
+    var campo = document.getElementById('valor')
+
+    // Faz a Request da API.
+    fetch(url, { method: 'GET' })
+        .then(function (response) {
+            response.json()
+                .then(function (dado) {
+                    campo.value = dado.BTC.ask;
+                });
+        })
+};
+
+function validar2() {
+    // Troca a logo pela Imagem da moeda
+    var moeda = document.getElementById('2').src;
+    document.getElementById('logo').src = moeda;
+
+    // Mostra a cotação.
+    var url = 'https://economia.awesomeapi.com.br/all/USD-BRL '
+    var campo = document.getElementById('valor')
+
+    // Faz a Request da API.
+    fetch(url, { method: 'GET' })
+        .then(function (response) {
+            response.json()
+                .then(function (dado) {
+                    campo.value = dado.USD.ask;
+                });
+        })
+};
+
+function validar3() {
+    // Troca a logo pela Imagem da moeda
+    var moeda = document.getElementById('3').src;
+    document.getElementById('logo').src = moeda;
+
+    // Mostra a cotação.
+    var url = 'https://economia.awesomeapi.com.br/all/CAD-BRL'
+    var campo = document.getElementById('valor')
+
+    // Faz a Request da API.
+    fetch(url, { method: 'GET' })
+        .then(function (response) {
+            response.json()
+                .then(function (dado) {
+                    campo.value = dado.CAD.ask;
+                });
+        })
+};
+
+function validar4() {
+    // Troca a logo pela Imagem da moeda
+    var moeda = document.getElementById('4').src;
+    document.getElementById('logo').src = moeda;
+
+    // Mostra a cotação.
+    var url = 'https://economia.awesomeapi.com.br/all/GBP-BRL'
+    var campo = document.getElementById('valor')
+
+    // Faz a Request da API.
+    fetch(url, { method: 'GET' })
+        .then(function (response) {
+            response.json()
+                .then(function (dado) {
+                    campo.value = dado.GBP.ask;
+                });
+        })
+};
+
+
+function validar5() {
+    // Troca a logo pela Imagem da moeda
+    var moeda = document.getElementById('5').src;
+    document.getElementById('logo').src = moeda;
+
+    // Mostra a cotação.
+    var url = 'https://economia.awesomeapi.com.br/all/EUR-BRL'
+    var campo = document.getElementById('valor')
+
+    // Faz a Request da API.
+    fetch(url, { method: 'GET' })
+        .then(function (response) {
+            response.json()
+                .then(function (dado) {
+                    campo.value = dado.EUR.ask;
+                });
+        })
+};
+
+function validar6() {
+    // Troca a logo pela Imagem da moeda
+    var moeda = document.getElementById('6').src;
+    document.getElementById('logo').src = moeda;
+
+    // Mostra a cotação.
+    var url = 'https://economia.awesomeapi.com.br/all/CHF-BRL '
+    var campo = document.getElementById('valor')
+
+    // Faz a Request da API.
+    fetch(url, { method: 'GET' })
+        .then(function (response) {
+            response.json()
+                .then(function (dado) {
+                    campo.value = dado.CHF.ask;
+                });
+        })
+};
+
+function validar7() {
+    // Troca a logo pela Imagem da moeda
+    var moeda = document.getElementById('7').src;
+    document.getElementById('logo').src = moeda;
+
+    // Mostra a cotação.
+    var url = 'https://economia.awesomeapi.com.br/all/JPY-BRL '
+    var campo = document.getElementById('valor')
+
+    // Faz a Request da API.
+    fetch(url, { method: 'GET' })
+        .then(function (response) {
+            response.json()
+                .then(function (dado) {
+                    campo.value = dado.JPY.ask;
+                });
+        })
+};
+
+function validar8() {
+    // Troca a logo pela Imagem da moeda
+    var moeda = document.getElementById('8').src;
+    document.getElementById('logo').src = moeda;
+
+    // Mostra a cotação.
+    var url = 'https://economia.awesomeapi.com.br/all/CNY-BRL '
+    var campo = document.getElementById('valor')
+
+    // Faz a Request da API.
+    fetch(url, { method: 'GET' })
+        .then(function (response) {
+            response.json()
+                .then(function (dado) {
+                    campo.value = dado.CNY.ask;
+                });
+        })
+};
